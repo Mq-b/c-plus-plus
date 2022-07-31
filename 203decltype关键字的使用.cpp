bb6a1576bcc1cb42f5929053183a4599de2c9eb3
guiguli <3326284481@qq.com>
2022-07-31 10:29:15 +0800

    decltype

---
+++ 203decltype关键字的使用.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+
+template <typename T>
+auto multiply(T x, T y)->decltype(x* y)//auto只是占位符，后面的decltype获取表达式运算后的类型作为返回值
+{
+	return x * y;
+}
+
+int main() {
+	decltype(1) a;//a是int
+
+	struct {
+		int a;
+		int b;
+	}c;//匿名类对象
+
+	decltype(c) d;//decltype得到类型再创建
+	d.a = 100;
+	std::cout << d.a << std::endl;
+
+	using type = decltype(c);//给匿名的类取别名
+	type e;//创建对象
+}
