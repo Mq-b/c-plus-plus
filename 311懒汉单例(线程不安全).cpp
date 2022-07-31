0f5df48ab0077bfbe3f07f2501dc9c8b31235527
guiguli <3326284481@qq.com>
2022-07-31 13:11:16 +0800

    311 312单例

---
+++ 311懒汉单例(线程不安全).cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+class singleton {
+private:
+	singleton() {}
+	static singleton* p;
+public:
+	static singleton* instance() {
+		if (p == nullptr)
+			p = new singleton;
+		return p;
+	}
+};
+
+singleton* singleton::p = nullptr;//类外定义初始化
+
+int main() {
+	auto a = singleton::instance();
+	delete a;
+}
---
+++ 312饿汉单例.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+class singleton {
+private:
+	singleton() {}
+	static singleton* p;
+public:
+	static singleton* instance() {
+		return p;
+	}
+};
+
+singleton* singleton::p = new singleton();//在定义的时候就实例化
+
+int main() {
+	auto a = singleton::instance();
+	delete a;
+}
+/*单例分为两种实现方法：
+	懒汉
+		第一次用到类实例的时候才会去实例化
+	饿汉
+		单例类定义的时候就进行了实例化。*/
