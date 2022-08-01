ef609f30ae71ede62098c81f6f6904ff9426c5cc
guiguli <3326284481@qq.com>
2022-08-01 12:11:24 +0800

    313 314 clock() GetTickCount64()

---
+++ 313clock()计时函数.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>  
+int main()
+{
+	double start, end, cost;
+	start = clock();
+	_sleep(100);
+	end = clock();
+	cost = end - start;
+	printf("%f\n", start);
+	printf("%f\n", end);
+	printf("%f\n", cost);//相减即可得出中间代码运行所需时间
+}
+/*
+* 在time.h文件中，还定义了一个常量CLOCKS_PER_SEC，它用来表示一秒钟会有多少个时钟计时单元，其定义如下：
+	#define CLOCKS_PER_SEC ((clock_t)1000)
+
+	表示clock（）可以精确到1毫秒
+
+	在linux系统下，CLOCKS_PER_SEC的值可能有所不同，目前使用的linux打印出来的值是1000000，表示的是微秒
+
+	lock()函数返回从“开启这个程序进程”到“程序中调用clock()函数”时之间的CPU时钟计时单元（clock tick）数
+
+	精度算较高，我们下面介绍GetTickCount()
+*/
---
+++ 314GetTickCount()计时函数.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+#include<Windows.h>
+int main() {
+	INT64 start = GetTickCount64();
+	_sleep(10);
+	INT64  end = GetTickCount64();
+	std::cout << "GetTickCount:" << end - start << std::endl;
+}
+/*
+	GetTickcount函数：它返回从操作系统启动到当前所经过的毫秒数(与lock不同)，它的返回值是DWORD。常常用来判断某个方法执行的时间，其函数原型是DWORD GetTickCount(void)，
+
+	返回值以32位的双字类型DWORD存储，因此可以存储的最大值是(2^32-1) ms约为49.71天，因此若系统运行时间超过49.71天时，这个数就会归0
+
+	特别注意：这个函数并非实时发送，而是由系统每18ms发送一次，因此其最小精度为(14-16)ms，也就是操作系统的一个CPU时间片。当需要有小于18ms的精度计算时，应使用StopWatch方法进行。
+*/
