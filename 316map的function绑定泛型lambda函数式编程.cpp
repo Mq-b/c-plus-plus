506f43f85d559ca0ee6b477e8a4b215a4083bc1a
guiguli <3326284481@qq.com>
2022-08-02 10:55:56 +0800

    316map的function绑定泛型lambda函数式编程

---
+++ 316map的function绑定泛型lambda函数式编程.cpp
@@ -0,0 +1,49 @@
+﻿#include<string>
+#include<map>
+#include<functional>
+#include<iostream>
+
+class Test {
+public:
+	std::map<int, std::function<int(int)>>func;
+	int m = 0;
+
+	Test() {
+		auto a = [this](auto n) {
+			m += 100;
+			show();
+			return n;
+		};
+		auto b = [this](auto n) {
+			m -= n;
+			show();
+			return n;
+		};
+		func.insert(std::make_pair(1, a));
+		func.insert(std::make_pair(2, b));
+	}
+
+	void test() {
+		auto a = [this](auto n) {
+			m += 100;
+			show();
+			return n;
+		};
+		std::function<std::string(std::string)>f(a);
+		auto b = f(std::string("傻子"));
+		std::cout << b << std::endl;
+	}
+private:
+	void show() {
+		std::cout << this->m << std::endl;
+	}
+};
+
+int main() {
+	Test a;
+	a.func[1](100);		//调用函数
+	std::cout << a.m << std::endl;
+
+	a.test();		//由此可见，function可以绑定任意的泛型lambda
+}
+//lambda是一个匿名类，重载了operator()且有template，所以可以这样做，详情看232
