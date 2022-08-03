5494e4a3a54947cc05cb083a5ffc98dfd977e075
guiguli <3326284481@qq.com>
2022-08-03 22:58:56 +0800

    323C++17std::visit

---
+++ 323C++17visit.cpp
@@ -0,0 +1,12 @@
+﻿#include<iostream>
+#include<variant>
+#include<string>
+
+int main() {
+	auto func = [](auto a) {std::cout << typeid(a).name() << std::endl; };
+	std::variant<int, double, std::string>var1(42), var2(3.14), var3("visit");
+
+	std::visit(func, var1);		//必须使用variant作为参数传递
+	std::visit(func, var2);
+	std::visit(func, var3);
+}
