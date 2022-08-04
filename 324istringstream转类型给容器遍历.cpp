b74980a08fe1b28b76c6b2cf53b3b8a9e1682fd1
guiguli <3326284481@qq.com>
2022-08-04 14:31:42 +0800

    324istringstream转类型给容器遍历

---
+++ 324istringstream转类型给容器遍历.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <sstream>
+#include <string>
+#include<vector>
+
+auto main()->int {
+	std::vector<int> V;
+	std::string s = "10 20 30 40 50 60 70 80 100";
+
+	std::istringstream out(s);
+	int i(0);
+	while (out >> i)//遍历放入容器
+		V.push_back(i);
+
+	std::ranges::copy(V, std::ostream_iterator<int>(std::cout, " "));
+}
