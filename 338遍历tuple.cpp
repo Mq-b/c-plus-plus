35a6be4d93723fa335cd28ac077a04cb6dbbc704
guiguli <3326284481@qq.com>
2022-08-11 09:27:39 +0800

    遍历tuple算法

---
+++ 338遍历tuple.cpp
@@ -0,0 +1,22 @@
+﻿#include<tuple>
+#include<iostream>
+#include<type_traits>
+
+template<class TupType, size_t... I>
+void print(const TupType& _tup, std::index_sequence<I...>)
+{
+	std::cout << "(";
+	(..., (std::cout << (I == 0 ? "" : ", ") << std::get<I>(_tup)));
+	std::cout << ")\n";
+}
+
+template<class... T>
+void print(const std::tuple<T...>& _tup)	//这个重载只是为了包装一下上面的，少输入一个参数
+{
+	print(_tup, std::make_index_sequence<sizeof...(T)>());
+}
+
+int main() {
+	auto tp = std::make_tuple(1, 3.14, "Hello Word");
+	print(tp);
+}
