0998f8caf16242837cb7051347c4a3a2a601e626
guiguli <3326284481@qq.com>
2022-08-11 22:39:46 +0800

    理解完全

--- 322any与泛型lambda.cpp
+++ 322any与泛型lambda.cpp
@@ -6,7 +6,8 @@
 int main() {
 
 	auto a = [](auto a, auto b) {return a + b; };
-	a(10, 20);					//我们能直接使用a可以猜测是lambda类返回了自己的对象
+	a(10, 20);					//我们能直接使用a 猜测是lambda匿名类返回了自己的对象,这个匿名类没有成员变量，是空类，所以a是一个字节
+	std::cout << sizeof(a) << std::endl;
 	using Type = decltype(a);	//获得泛型lambda的类型 lambda是一个匿名类
 	auto i = Type()(10, 20);	//这相当于直接先构造了一个对象，然后调用了operator()
 	std::cout<<i<<std::endl;
@@ -22,3 +23,4 @@ int main() {
 	V.push_back(100);	//any可以接收任何类型
 	std::cout << std::any_cast<int>(V[1]) << std::endl;	//使用std::any_cast将类型转换回来即可
 }
+//不能对进行了捕获的lambda进行这些操作 不管是& 还是=
