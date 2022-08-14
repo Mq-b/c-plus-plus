0aea73ebd277270e3d5da8d1e83ef39f5f775670
guiguli <3326284481@qq.com>
2022-08-14 10:57:19 +0800

    342std::equal

---
+++ 342equal检查字符串是否回文.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+void test(const std::string_view& s)
+{
+    if (std::equal(s.begin(), s.begin() + s.size() / 2, s.rbegin())) 
+        std::cout << s << " 是回文串" << std::endl;
+    else 
+        std::cout << s << " 不是回文串" << std::endl;
+}
+int main()
+{
+    test("radar");
+    test("hello");
+}
