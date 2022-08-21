0cba25320a4ccbe88bda1a3a13e4ac790a2d929c
归故里 <97590219+13870517674@users.noreply.github.com>
2022-08-21 16:18:18 +0800

    修改357，自定义异常类

--- 357C++标准异常与cerr等.cpp
+++ 357C++标准异常与cerr等.cpp
@@ -1,9 +1,16 @@
 #include<iostream>
 #include<exception>
+#include<string>
+
+struct MyException :std::exception
+{
+	MyException(const std::string &s) :exception(s.data()) {}//没必要重写what方法，无所谓，看需求即可，不知道怎么写就看源码实现
+};
+
 void f()noexcept(false) {
 	try
 	{
-		throw std::runtime_error("6666");
+		throw std::runtime_error("6666");//运行时异常
 		//throw std::logic_error("错误");//抛出多个异常也只有第一个会被捕获，所以这里注释
 	}
 	catch (const std::exception& e)//c++的标准异常类，其他异常都继承自它，所以这里是父类能够捕获所有子类异常(多态)
@@ -11,8 +18,18 @@ void f()noexcept(false) {
 		std::cerr << e.what() << std::endl;//std::cerr是c++输出到标准错误的对象
 	}
 }
+//自定义异常
+void ff()noexcept(false) {
+	try {
+		throw MyException("错误错误!");
+	}catch (const std::exception& e) {
+			std::cerr << e.what() << std::endl;
+	}
+}
 int main() {
-	f();
+	//f();
+	ff();
+
 	//throw std::logic_error("傻子");//就算不捕获这个异常它依旧会打印到标准错误然后才会中断
 	std::cout << "6666" << std::endl;
 
