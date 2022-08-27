a66f6286493c278e1eb0eaad1b91dec56fc0f10f
归故里 <97590219+13870517674@users.noreply.github.com>
2022-08-27 19:06:19 +0800

    std::remove_reference的简单实现

--- 367remove_reference将任何引用转换为原始类型.cpp
+++ 367remove_reference将任何引用转换为原始类型.cpp
@@ -6,6 +6,23 @@ void print_is_same() {
 	std::cout << std::is_same_v<T1, T2> << '\n';
 }
 
+//它的简单实现
+template<class T>
+struct Test {
+	using type = T;
+};
+template<class T>
+struct Test<T&> {
+	using type = T;
+};
+template<class T>
+struct Test<T&&> {
+	using type = T;
+};
+
+template<class T>
+using Test_t = typename Test<T>::type;
+
 int main() {
 	std::cout << std::boolalpha;
 
@@ -17,4 +34,9 @@ int main() {
 	print_is_same<int, std::remove_reference_t<int&>>();
 	print_is_same<int, std::remove_reference_t<int&&>>();
 	//std::remove_reference_t的作用非常简单，类型不管是左值还是右值都变成引用的类型的，即:就是T&& T& T通通变成T
+
+	using a = Test_t<int&>;//int
+	using b = Test_t<int&&>;//int
+	using c = Test_t<int>;//int
+	std::cout << typeid(a).name() << "," << typeid(b).name() << "," << typeid(c).name() << std::endl;
 }
