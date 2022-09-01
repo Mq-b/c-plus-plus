cfccfb3626159809c9e22292eab906ec43d674db
归故里 <97590219+13870517674@users.noreply.github.com>
2022-09-01 15:41:53 +0800

    370requires判断类型是否为多态类型

---
+++ 370使用约束requires判断类型是否为多态类型.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include <vector>
+
+template<typename Type>//思想就是非多态类型使用dynamic_cast会报错，也就是不是可行的语句，requires返回false
+inline constexpr bool is_polymorphic_v =
+	requires{
+	dynamic_cast<const volatile void*>(static_cast<Type*>(nullptr));
+};
+
+int main() {
+	auto a = is_polymorphic_v<std::exception>;
+	auto b = is_polymorphic_v<std::vector<int>>;
+}
