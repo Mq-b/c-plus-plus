46b995b8f66a4a05c0a9539b9b10724fc64acf16
归故里 <97590219+13870517674@users.noreply.github.com>
2022-09-04 09:36:53 +0800

    复制消除

--- src/304类构造的各种优化_复制消除.cpp
+++ src/304类构造的各种优化_复制消除.cpp
@@ -10,9 +10,12 @@ public:
 	}
 	void operator=(Person& n) {
 		this->a = n.a;
-		std::cout << "拷贝构造" << std::endl;
+		std::cout << "operator=" << std::endl;
 	}
-
+	Person(Person&&p) {
+		this->a = p.a;
+		std::cout << "移动构造" << std::endl;
+	}
 	int a = 0;
 };
 
@@ -27,3 +30,5 @@ int main() {
 	//总结 对于编译器来说 【类名 对象名(参数)】 和 【类名 对象名=参数】调用的是同一个有参构造函数 是一个意思
 	//且Person b = Person();这种会直接优化为默认构造
 }
+/*这里其实是c++11的复制消除
+https://zh.cppreference.com/w/cpp/language/copy_elision*/
