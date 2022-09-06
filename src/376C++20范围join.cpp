f7f355de2d9a288545388767e25d4e9343f12d33
归故里 <97590219+13870517674@users.noreply.github.com>
2022-09-06 14:17:10 +0800

    拉平范围的视图(多层)

--- src/376C++20范围join.cpp
+++ src/376C++20范围join.cpp
@@ -17,6 +17,13 @@ int main()
 
     int array[5][5]{ 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25 };
     for (int c : array | std::views::join)std::cout << c<<' ';
+    std::cout << '\n';
+
+    //默认只能处理两层，如果多层，那么就多join几次
+    int array2[2][2][2]{ 1,2,3,4,5,6,7,8 };
+    for (int c : std::ranges::join_view(array2) | std::views::join)std::cout << c << ' ';
+    std::cout << '\n';
+
 }
 /*1) 表示由从拉平范围的视图获得的序列组成的 view 。
 2) 范围适配器对象。对于任何适合的子表达式 e 表达式 views::join(e) 表达式等价于 join_view<views::all_t<decltype((e))>>{e}*/
