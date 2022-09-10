76c68dbeef782f582d944da0ae1d767b15a5d041
归故里 <97590219+13870517674@users.noreply.github.com>
2022-09-10 23:29:30 +0800

    380std::underlying_type_t的使用与枚举

--- src/375C++20管道运算符filter筛选.cpp
+++ src/375C++20管道运算符filter筛选.cpp
@@ -6,7 +6,6 @@ int main()
     auto const ints = { 0,1,2,3,4,5 };
     auto even = [](int i) { return 0 == i % 2; };
     auto square = [](int i) { return i * i; };
-
     // 组合视图的“管道”语法：
     for (int i : ints | std::views::filter(even) | std::views::transform(square)) {
         std::cout << i << ' ';
