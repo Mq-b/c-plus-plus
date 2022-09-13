8b203634aa8e30fe9b25f8fe4c0ff71987211f41
归故里 <97590219+13870517674@users.noreply.github.com>
2022-09-13 20:55:05 +0800

    静态成员初始化器访问可访问类内私有保护成员

--- src/312饿汉单例.cpp
+++ src/312饿汉单例.cpp
@@ -9,8 +9,7 @@ public:
 	}
 };
 
-singleton* singleton::p = new singleton();//在定义的时候就实例化
-
+singleton* singleton::p = new singleton();//静态成员的初始化器可以访问类的私有与保护成员,所以这里可以使用new定义并初始化静态成员
 int main() {
 	auto a = singleton::instance();
 	delete a;
