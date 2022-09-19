45b32b820b256e3010448a9fb4f8f20db86bbd2e
归故里 <97590219+13870517674@users.noreply.github.com>
2022-09-19 16:34:34 +0800

    new表达式中使用auto()，与operator auto()

--- src/392auto()的详细使用.cpp
+++ src/392auto()的详细使用.cpp
@@ -1,19 +1,29 @@
 #include<iostream>
 struct X {
-	operator std::integral auto() {//这其实也是一种转换函数，转换函数不需要写返回值类型
+	 operator std::integral auto() {//这其实也是一种转换函数，转换函数不需要写返回值类型
 		return 10000;
 	}
 };
+
 int main() {
 	double* p = new double[] {1, 2, 3}; // 创建 double[3] 类型的数组
 	auto p2 = new auto('c');          // 创建单个 char 类型的对象。p 是一个 char*
-
 	auto q = new std::integral auto(1);         // OK: q 是一个 int*
 	//auto q = new std::floating_point auto(true) // 错误：不满足类型约束
-
 	auto r = new std::pair(10, true); // OK: r 是一个 std::pair<int, bool>*
+
 	auto r2 = new auto(X{});
 	std::cout << *r2 << std::endl;
+	
+	X c;
+	std::cout << int(c) + 1 << std::endl;
+	decltype(auto) r4 = *r2;
+	std::cout << int(r4) + 10 << std::endl;
 }
-//在new的时候，我们不能对表达式使用decltype，然后new，但是可以auto，比如new auto(1)
+//在new的时候，可以auto，比如new auto(1),也就是new一个int
 //约束这个事情，比如std::integral，这些都是c++20的concept,它要求的是返回值类型必须是int
+
+//operator std::integral auto(),std::integral要求返回值类型是int。另外msvc是有bug的，明明没有用explicit修饰，它不会进行隐式转换，必须显式的，
+//但是如果使用cout，它又能隐式转换了，比如16行这个，如果你使用的是gcc，那么19和21行的显式转换都不需要加，可以隐式转换
+
+//注意:转换函数模板不允许具有推导的返回值类型
