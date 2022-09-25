3dca8e2600e5f61dbfe9ff09756514550a1d9262
归故里 <97590219+13870517674@users.noreply.github.com>
2022-09-25 19:48:08 +0800

    增加对std::apply无法传入函数模板的解决方案

--- src/404C++17apply.cpp
+++ src/404C++17apply.cpp
@@ -27,8 +27,16 @@ int main()
 {
     std::cout << std::apply(add, std::tuple(1, 2)) << '\n';//3
 
-    // 错误：无法推导函数类型
+    // 错误：函数模板无法推导函数类型
     //std::cout << std::apply(add_generic, std::pair(2.0, 3.0f)) << '\n'; 
+    //解决方案，1:转函数指针
+    std::cout << std::apply(static_cast<int(*)(int, int)>(add_generic), std::pair(7, 8)) << std::endl;
+
+    //或者直接指明std::apply的类型为具体函数类型，而非函数指针(其实指明函数指针类型也可)
+    std::cout << std::apply<int(int, int)>(add_generic, std::pair(7, 8)) << std::endl;
+
+    //最简单的方式是直接指明模板函数的类型
+    std::cout << std::apply(add_generic<int>, std::pair(7, 8)) << std::endl;
 
     // OK
     std::cout << std::apply([](auto first, auto second) { return first + second; }, std::pair(2.0f, 3.0f)) << '\n';//5
