f336f983cdd1a1593e2f0cfb7d752c0a5e769f00
归故里 <97590219+13870517674@users.noreply.github.com>
2022-09-28 14:09:18 +0800

    Boost库

---
+++ Boost/Boost的基本使用/src/01boost初识大数计算.cpp
@@ -0,0 +1,16 @@
+#include <boost/multiprecision/cpp_dec_float.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
+#include <boost/multiprecision/gmp.hpp>
+#include <iostream>
+
+int main() {
+    using Int = boost::multiprecision::cpp_int;
+    using Float = boost::multiprecision::number<boost::multiprecision::gmp_float<0>>;
+
+    Float fake_pi;
+    boost::multiprecision::default_ops::calc_pi(fake_pi.backend(), 2000);
+
+    Int value("12345678901234567890123456789012345678901234567890");
+
+    std::cout << std::fixed << value << " * " << fake_pi << " = " << Float(value.convert_to<Float>() * fake_pi);
+}
