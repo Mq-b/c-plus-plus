4364d0ba03333ab5f2ba98ce3770a5450e06423e
归故里 <97590219+13870517674@users.noreply.github.com>
2022-10-11 14:13:26 +0800

    命名空间作用域内未声明为 static 或 const(包括constexpr) 变量都具有内部链接

--- src/421命名空间作用域和静态数据成员拥有外部链接.cpp
+++ src/421命名空间作用域和静态数据成员拥有外部链接.cpp
@@ -2,6 +2,7 @@
 
 namespace S {
 	extern int n;
+	extern const int v;//命名空间作用域内未声明为 static 或 const(包括constexpr) 变量都具有内部链接
 	int f();
 }
 struct X {//本翻译单元的定义，只是为了去查找外部链接的value
@@ -10,6 +11,7 @@ struct X {//本翻译单元的定义，只是为了去查找外部链接的value
 int main() {
 	std::cout << S::n << std::endl;
 	std::cout << S::f() << std::endl;
+	//std::cout << S::v << std::endl;//error 无法打印
 	std::cout << X::value << std::endl;
 }
 // 参考: https://zh.cppreference.com/w/cpp/language/storage_duration
--- src/static_data_member.cpp
+++ src/static_data_member.cpp
@@ -6,5 +6,6 @@ int X::value = 10;
 
 namespace S {//命名空间作用域都具有外部链接
 	int n = 10;
+	const int v = 10;
 	int f() { return 6; };
 }
