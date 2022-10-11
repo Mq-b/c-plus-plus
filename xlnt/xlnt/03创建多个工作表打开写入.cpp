6f003f0634fe537c73fac94bd6902e0854ca7d59
归故里 <97590219+13870517674@users.noreply.github.com>
2022-10-11 09:27:39 +0800

    创建多个sheet打开并写入

---
+++ xlnt/xlnt/03创建多个工作表打开写入.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<xlnt/xlnt.hpp>
+
+int main() {
+	xlnt::workbook wb;
+	//创建1个表格,注意使用active_sheet是返回一个活跃的工作表，无法多创建
+	wb.active_sheet().title("data_2");
+	//根据title打开工作表
+	auto ws = wb.sheet_by_title("data_2");
+	ws.cell("A1").value(2);
+
+	//使用create_sheet()方法才可创建多个工作表
+	wb.create_sheet().title("data_1");
+	auto ws2 = wb.sheet_by_title("data_1");
+	ws2.cell("A1").value(1);
+
+	wb.save("test_data.xlsx");
+}
