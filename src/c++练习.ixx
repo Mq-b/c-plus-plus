18d78415c2adf66641dc112000570bddef1b1a54
归故里 <97590219+13870517674@users.noreply.github.com>
2022-12-28 13:26:32 +0800

    小修改

--- src/c++练习.ixx
+++ src/c++练习.ixx
@@ -1,10 +1,5 @@
 ﻿#include"lib/sundry.hpp"
-#include<numbers>
-
-void f(int n) {
-	std::cout << n << '\n';
-}
 
 int main() {
-	Call(f, int)(1);
+
 }
--- src/lib/sundry.hpp
+++ src/lib/sundry.hpp
@@ -328,8 +328,8 @@ namespace mylib {
 	}
 
 }
-#define STD       ::std::
-#define LOG	      ::my::singleton<my::clog>::get().object
+#define STD        ::std::
+#define LOG	       ::my::singleton<my::clog>::get().object
 
 #define Fu(item)					\
 	{								\
@@ -338,14 +338,14 @@ namespace mylib {
 
 #define Call(func,...) (::my::F_<func , __VA_ARGS__>)
 
-namespace stdr = ::std::ranges;
-namespace stdv = ::std::views;
-namespace stdf = ::std::filesystem;
-namespace stdl = ::std::literals;
-namespace stdc = ::std::chrono;
-namespace stdp = ::std::pmr;
-namespace stde = ::std::experimental;
-namespace my = ::mylib;
+namespace stdr   = ::std::ranges;
+namespace stdv   = ::std::views;
+namespace stdf   = ::std::filesystem;
+namespace stdl   = ::std::literals;
+namespace stdc   = ::std::chrono;
+namespace stdp   = ::std::pmr;
+namespace stde   = ::std::experimental;
+namespace my     = ::mylib;
 namespace mylibf = ::mylib::file;
 
 using namespace my::literals;
