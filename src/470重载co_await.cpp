1247f159b927eb8c2f53323759ed0058ce9afe41
归故里 <3326284481@qq.com>
2023-06-06 19:54:12 +0800

    修改对co_await重载的协程的demo，那个线程用的有问题，改改

--- src/470重载co_await.cpp
+++ src/470重载co_await.cpp
@@ -1,6 +1,8 @@
 #include <coroutine>
 #include <iostream>
 #include<thread>
+#include<chrono>
+using namespace std::literals;
 
 struct promise;
 struct coroutine : std::coroutine_handle<promise> {
@@ -9,13 +11,13 @@ struct coroutine : std::coroutine_handle<promise> {
 
 struct promise {
 	int n;
-	coroutine get_return_object()//返回值类型要求可以类型::promise_type
+	coroutine get_return_object()
 	{
 		return { coroutine::from_promise(*this) };
 	}
-	std::suspend_never initial_suspend() noexcept { return {}; }//注意返回类型
+	std::suspend_never initial_suspend() noexcept { return {}; }
 	std::suspend_always final_suspend() noexcept { return {}; }
-	std::suspend_always yield_value(int r) { n = r; return {}; }//co_yield()需要
+	std::suspend_always yield_value(int r) { n = r; return {}; }
 	void return_void() {}
 	void unhandled_exception() {}
 };
@@ -23,36 +25,42 @@ struct promise {
 template<typename T>
 struct Future {
 	T n;
+	Future(T v) :n{ v } { puts("Future"); }
+	~Future() { puts("~Future"); }
 	bool await_ready() {
 		return false;
 	}
 	void await_suspend(std::coroutine_handle<coroutine::promise_type>h) {
-		std::jthread thread{[this, &h] {h.resume(); } };//协程在子线程运行
-		//协程暂停后跳转到哪 void返回类型是暂停后跳转到调用到调用者
+		std::thread thread{[this, h] {//创建异步任务
+			int t = this->n;
+			for (int i = 1; i < t; ++i)
+				this->n *= i;
+			if (!h.done())h.resume();//执行完任务之后重新启动协程
+		}};
+		thread.detach();
 	}
-	T await_resume() { return {}; }//调用co_wait的返回值 await_resume的返回值是整个co_await表达式的结果
+	T await_resume() { return n; }
 };
 
 template<typename T>
-inline auto operator co_await(Future<T> x) {//必须返回类类型 operator co_await的返回类型和co_await表达式没任何关系
+inline auto operator co_await(const Future<T>& x) {
 	std::cout << "operator co_await 被调用 X= " << x.n << '\n';
-	return x;//目前这个返回值没啥用,co_await的返回值送进协程调度器的,我们不会使用，目前的Future其实给调度器也没啥意义，只是表示可以这么写
+	return x;
 }
 
 coroutine f() {
-	int v = co_await Future{ 10 };
-	std::cout << "thread ID: " << std::this_thread::get_id() << '\n';//子线程ID
-	std::cout << v << '\n';
+	Future fu{ 5 };
+	int v = co_await fu;
+	std::cout << "n: " << v << '\n';
+
 	co_yield 20;
-	std::cout << "thread ID: " << std::this_thread::get_id() << '\n';//主线程ID
 }
 int main() {
-	std::cout << "main thread ID: " << std::this_thread::get_id() << '\n';
 	auto result = f();
 	std::cout << "main\n";
+	std::this_thread::sleep_for(100ms);
 	result.resume();
-	std::cout << result.promise().n << '\n';//可以通过promise()方法访问我们写的promise类的成员
 	std::cout << std::boolalpha << result.done() << '\n';
+	std::cout << "co_yield value: " << result.promise().n << '\n';
 }
-//写模板纯属为了展示可以写模板
-//https://eel.is/c++draft/expr.await#6
+//我只要暂停协程回到调用者就会析构我的局部对象
