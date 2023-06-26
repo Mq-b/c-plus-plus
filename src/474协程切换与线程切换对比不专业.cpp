f11390efd14c7ad82d7564e298264bbf0694af82
归故里 <3326284481@qq.com>
2023-06-26 12:09:35 +0800

    更改协程切换效率测试

--- src/474协程切换与线程切换对比不专业.cpp
+++ src/474协程切换与线程切换对比不专业.cpp
@@ -1,49 +1,59 @@
 #include<iostream>
-#include<experimental/generator>
 #include<chrono>
 #include<future>
 #include<thread>
+#include<experimental/generator>
 using namespace std::literals;
 
-std::mutex mtx{};//共用这一个锁
+std::mutex mtx{};
 std::condition_variable cond{};
+std::condition_variable cond2{};
+bool flag = true;
 int v{ 0 };
 
 void func(int n) {
 	for (int i = 0; i < n; ++i) {
 		std::unique_lock lck{ mtx };
-		cond.wait(lck, [=] {return i == v; });
-		std::cout << i << '\n';
+		cond.wait(lck, [=] {
+			if (flag) { flag = false; return i == v; }
+			else return false;
+			});
+		//std::cout << i << '\n';
+		cond2.notify_all();
 	}
 }
 
 void call() {
 	auto start = std::chrono::steady_clock::now();
-	std::thread t{func, 10};
-	for (int i = 0; i < 10; ++i) {
-		std::this_thread::sleep_for(100ms);
-		std::lock_guard x{ mtx };
+	std::thread t{func, 10000000};
+	for (int i = 0; i < 10000000; ++i) {
+		std::unique_lock x{ mtx };
 		v = i;
+		flag = true;
 		cond.notify_all();
+		cond2.wait(x);
 	}
 	t.join();
 	auto end = std::chrono::steady_clock::now();
 	std::cout << std::chrono::duration<double>(end - start) << '\n';
 }
-
-std::experimental::generator<int> coroutine(int i) {
-	for (int i = 0; i < 10; ++i) {
+std::experimental::generator<int> coroutine(int n) {
+	for (int i = 0; i < n; ++i) {
 		co_yield i;
 	}
 }
-
 int main() {
+	call();
+
 	auto start = std::chrono::steady_clock::now();
-	for (auto i : coroutine(10)) {
-		std::cout << i << '\n';
+	for (auto i : coroutine(10000000)) {
+		//std::cout << i << '\n';
 	}
 	auto end = std::chrono::steady_clock::now();
 	std::cout << std::chrono::duration<double>(end - start) << '\n';
-
-	call();
 }
+
+/*
+1000万次线程切换：7.3905s 平均每次739.05ns
+1000万次协程切换0.665018s
+*/
